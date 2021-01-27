cdb70cb4ba4a5fa449efdb29df56b9ca5e741363
Abhilash <Abhilash@DESKTOP-UNJFCRG>
2021-01-27 09:09:06 +0530

    Added AuthorController class

---
+++ MySpringProject/src/main/java/com/example/MySpringProject/Controllers/AuthorController.java
@@ -0,0 +1,28 @@
+package com.example.MySpringProject.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.MySpringProject.Repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+	
+	private final AuthorRepository authorRepository;
+
+	public AuthorController(AuthorRepository authorRepository) {
+		super();
+		this.authorRepository = authorRepository;
+	}
+	
+	@RequestMapping("/authors")
+	public String getAuthors(Model model)
+	{
+		model.addAttribute("authors",authorRepository.findAll());
+		return "authorsList";
+	}
+	
+	
+
+}
